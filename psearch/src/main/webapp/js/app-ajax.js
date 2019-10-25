0bda31866910fe615c60e0ffcfc322054e7795f5
aurona <pablo.delahorra@gmail.com>
2019-10-25 15:18:25 +0200

    html passing parameters

--- psearch/src/main/webapp/js/app-ajax.js
+++ psearch/src/main/webapp/js/app-ajax.js
@@ -1,25 +1,33 @@
     <!-- Scripts to execute actions -->
 
     function getSchema() {
-      var xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-          document.getElementById("res").innerHTML =
-          this.responseText;
-        }
-      };
-      xhttp.open("GET", "https://java-gcs-256106.appspot.com/hello?order=getschema", true);
-      xhttp.send();
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("res").innerHTML =
+                this.responseText;
+                }
+        };
+        xhttp.open("GET", "https://java-gcs-256106.appspot.com/hello?order=getschema", true);
+        xhttp.send();
     }
 
-    function updateSchema() {
-      var xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-          document.getElementById("res").innerHTML =
-          this.responseText;
-        }
-      };
-      xhttp.open("GET", "https://java-gcs-256106.appspot.com/hello?order=updateschema", true);
-      xhttp.send();
+    function updateSchema(datasourceid) {
+        data: document.getElementsByTagName("textarea") [0].value
+        alert("id: " + datasourceid);
+
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("res").innerHTML =
+                this.responseText;
+                }
+        };
+        xhttp.open("GET", "https://java-gcs-256106.appspot.com/hello?order=updateschema", true);
+        xhttp.send();
+    }
+
+
+    function test(datasourceid) {
+        alert("dsid: " + datasourceid);
     }
